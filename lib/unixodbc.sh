857b02fbb40c7e8bcbe942a827a4730811af3472
Cody Coons <cody@immuta.com>
2016-11-29 15:13:46 -0500

    Fixed bug with attr in curl

--- lib/unixodbc.sh
+++ lib/unixodbc.sh
@@ -4,7 +4,7 @@ install_unixodbc() {
 
   local download_url="http://www.unixodbc.org/unixODBC-$version.tar.gz"
   echo "Downloading and installing unixODBC at $download_url"
-  curl "$download_url" --silent --fail --retry 5 --retry-max-time 15 0x /tmp/unixodbc.tar.gz || (echo "Unable to download unixODBC" && false)
+  curl "$download_url" --silent --fail --retry 5 --retry-max-time 15 -o /tmp/unixodbc.tar.gz || (echo "Unable to download unixODBC" && false)
   tar xzf /tmp/unixodbc.tar.gz -C /tmp
   ls /tmp
 }
